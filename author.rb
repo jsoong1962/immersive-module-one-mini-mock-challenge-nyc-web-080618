3076b33f20f885afbef19391361ae948643069c0
Johnny <johnnysoong@hotmail.com>
2018-08-13 00:07:33 -0400

    mock completed

--- author.rb
+++ author.rb
@@ -1,1 +1,40 @@
+class Author
+  attr_reader :name
+  @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+def books
+  Book.all.select do |book|
+    book.author == self
+  end
+  end
+
+  def write_book(title, word_count)
+    Book.new(self, title, word_count)
+  end
+
+  def total_words
+    total = 0
+    books.each do |book|
+      total += book.word_count
+    end
+    total
+end
+
+
+def self.most_words
+  array = Book.all.sort do |book1, book2|
+  book2.word_count <=> book1.word_count
+  end
+  array[0].author
+  end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,13 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+johnny = Author.new("Johnny")
+david = Author.new("David")
+lisa = Author.new("Lisa")
+
+book1 = Book.new(johnny, "Jaws", 10000)
+book2 = Book.new(david, "Attack", 20000)
+book3 = Book.new(lisa, "Yield", 30000)
+book4 = Book.new(johnny, "Fuck", 30000)
 binding.pry
-
-
